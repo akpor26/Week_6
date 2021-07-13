95a3640ee073a7e39a3d336ecdf4858edffd0684
akpor26 <onovughe26@gmail.com>
2021-07-13 18:18:06 +0100

    week6 kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,29 @@
 fun main() {
-    var inLove = loveCalculator(4,4)
-    println("Are they in love? $inLove")
+    var inLuv = loveCaller(4,4)
+    println("Are they in love? $inLuv")
 
-    inLove = loveCalculator(3,11)
-    println("Are they in love? $inLove")
+    inLuv = loveCaller(3,11)
+    println("Are they in love? $inLuv")
 
-    inLove = loveCalculator(22,17)
-    println("Are they in love? $inLove")
+    inLuv = loveCaller(22,17)
+    println("Are they in love? $inLuv")
 
-    //tried something different
+
     println()
-    println("Using loveCal function")
-    loveCal(4,4)
-    loveCal(3,11)
-    loveCal(22,17)
+    println("Using lover function")
+    lover(4,4)
+    lover(3,11)
+    lover(22,17)
 
 
 }
 
-fun loveCalculator(flower1: Int, flower2: Int): Boolean{
-    return(flower1 % 2 != flower2 % 2)
+fun loveCaller(lovebird1: Int, lovebird2: Int): Boolean{
+    return(lovebird1 % 2 != lovebird2 % 2)
 }
 
-fun loveCal(flower1: Int, flower2: Int){
-    var result = if(flower1 % 2 != flower2 % 2){
+fun lover(lovebird1: Int, lovebird2: Int){
+    var result = if(lovebird1 % 2 != lovebird2 % 2){
         println("true")
     }else{
         println("false")
